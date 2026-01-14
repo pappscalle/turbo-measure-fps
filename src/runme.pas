a3f0f340033a0e57faaa05e2cfa8db10336f2dec
Calle <carl.frenn@gmail.com>
2026-01-14 17:17:55 +0100

    Boxes ad object with and drawing with assembler

--- src/runme.pas
+++ src/runme.pas
@@ -8,43 +8,80 @@ const
   NUM_BOXES = 5;
 
 type 
-  TBox = record
+  TBox = object
     x, y : integer;
     dx, dy : integer;
     color : byte;
+    procedure Init;
+    procedure Move;
+    procedure Draw;
   end;
-
-
 
 var 
   i : integer;
   boxes : array [0..NUM_BOXES-1] of TBox;
-
   fpsCounter: TFpsCounter;
-
   s: string[10];
 
-
-
-procedure DrawBox(var b: TBox);
-var ix, iy: integer;
+procedure TBox.Init;
 begin
-  for iy := b.y to b.y + 15 do
-    for ix := b.x to b.x + 15 do
-      SetPixel(ix, iy, b.color);
+  x := random(SCREEN_WIDTH - 16);
+  y := random(SCREEN_HEIGHT - 16);
+  dx := random(3) + 1;
+  dy := random(3) + 1;
+  color := random(16);
 end;
+
+procedure TBox.Move;
+begin
+  x := x + dx;
+  y := y + dy;
+
+  if (x < 0) or (x > SCREEN_WIDTH - 16) then
+    dx := -dx;
+  if (y < 0) or (y > SCREEN_HEIGHT - 16) then
+    dy := -dy;
+end;
+
+procedure TBox.Draw; assembler;
+asm
+  push ds
+
+  mov ax, $A000
+  mov es, ax
+
+  lds si, Self
+
+  mov bx, [si]            { x }
+  mov dx, [si+2]          { y }
+  mov al, [si+8]          { color }
+
+  mov di, dx
+  shl di, 6
+  mov cx, dx
+  shl cx, 8
+  add di, cx
+  add di, bx
+
+  mov cx, 16
+@row:
+  push cx
+  mov cx, 16
+  rep stosb
+  add di, 320 - 16
+  pop cx
+  loop @row
+
+  pop ds
+end;
+
+
 
 begin
 
   randomize;
 
-  for i := 0 to NUM_BOXES-1 do begin
-    boxes[i].x := random(SCREEN_WIDTH - 16);
-    boxes[i].y := random(SCREEN_HEIGHT - 16);
-    boxes[i].dx := random(3) + 1;
-    boxes[i].dy := random(3) + 1;
-    boxes[i].color := random(16);
-  end;
+  for i := 0 to NUM_BOXES-1 do boxes[i].Init;
 
   fpsCounter.Init;
   
@@ -54,22 +91,14 @@ begin
     {ClearScreen(0);}
  
     for i := 0 to NUM_BOXES-1 do begin
-      DrawBox(boxes[i]);
-
-      boxes[i].x := boxes[i].x + boxes[i].dx;
-      boxes[i].y := boxes[i].y + boxes[i].dy;
-
-      if (boxes[i].x < 0) or (boxes[i].x > SCREEN_WIDTH - 16) then
-        boxes[i].dx := -boxes[i].dx;
-
-      if (boxes[i].y < 0) or (boxes[i].y > SCREEN_HEIGHT - 16) then
-        boxes[i].dy := -boxes[i].dy;
+      boxes[i].Move;
+      boxes[i].Draw;
     end;  
 
     fpsCounter.Frame;  
     fpsCounter.Update;
 
-    Str(fpsCounter.fps, s);
+    Str(fpsCounter.fps:2, s);
     DrawString(0, 180, s, 15);
 
   until KeyPressed;
